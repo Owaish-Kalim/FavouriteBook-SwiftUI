f1414db8c96bf333d686b0e01c016054ff9724bd
Owaish <owaishkalim@gmail.com>
2021-08-26 13:01:09 +0530

    Initial Commit

---
+++ FavouriteBook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FavouriteBook
+//
+//  Created by owaish on 26/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
